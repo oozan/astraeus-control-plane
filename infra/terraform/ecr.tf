cfdbfde0d46262aa53db1f4361cf9bca5e4b45b3
oozan <oozayranci@gmail.com>
2026-02-28 18:26:13 +0200

    infra(terraform): add ecr repositories for services

---
+++ infra/terraform/ecr.tf
@@ -0,0 +1,21 @@
+resource "aws_ecr_repository" "api" {
+  name                 = "${local.name_prefix}-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_ecr_repository" "worker" {
+  name                 = "${local.name_prefix}-worker"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = local.common_tags
+}
